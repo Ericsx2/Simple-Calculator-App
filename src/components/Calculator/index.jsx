fba3ed32b5e27c16a2a11857ed7525555a65d236
Eric Soares <ericsoares51@hotmail.com>
2020-07-22 19:31:20 -0300

    Ajust of props

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -22,23 +22,23 @@ function Calculator() {
 
   return <Container>
     <Display value="100"/>
-    <Button label="AC" click={() => clearMemory()} />
-    <Button label="/" click={n => setOperation(n)} />
+    <Button label="AC" click={() => clearMemory()} triple/>
+    <Button label="/" click={n => setOperation(n)} operation/>
     <Button label="7" click={n => addDigit(n)} />
     <Button label="8" click={n => addDigit(n)} />
     <Button label="9" click={n => addDigit(n)} />
-    <Button label="*" click={n => setOperation(n)} />
+    <Button label="*" click={n => setOperation(n)} operation/>
     <Button label="4" click={n => addDigit(n)} />
     <Button label="5" click={n => addDigit(n)} />
     <Button label="6" click={n => addDigit(n)} />
-    <Button label="-" click={n => setOperation(n)} />
+    <Button label="-" click={n => setOperation(n)} operation/>
     <Button label="1" click={n => addDigit(n)} />
     <Button label="2" click={n => addDigit(n)} />
     <Button label="3" click={n => addDigit(n)} />
-    <Button label="+" click={n => setOperation(n)} />
-    <Button label="0" click={n => addDigit(n)} />
+    <Button label="+" click={n => setOperation(n)} operation/>
+    <Button label="0" click={n => addDigit(n)} double/>
     <Button label="." click={n => addDigit(n)} />
-    <Button label="=" click={n => setOperation(n)} />
+    <Button label="=" click={n => setOperation(n)} operation/>
   </Container>
 }
 
