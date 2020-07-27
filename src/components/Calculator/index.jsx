9552166339f2470cb8b78b09d6384c838ee8615a
Eric Soares <ericsoares51@hotmail.com>
2020-07-27 15:36:05 -0300

    .

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -15,17 +15,67 @@ const initialState = {
 function Calculator() {
 
   const [state, setState] = useState(initialState)
-
+  
   function clearMemory() {
     setState(initialState)
   }
 
   function setOperation(n) {
-    console.log(n)
+    if ( state.current === 0) {
+      setState({operation: n, current: 1, clearDisplay: true})
+    }else{
+      const equals = n === '='
+      const currentOperation = state.operation
+      
+      const values = [...state.values]
+      switch(currentOperation){
+        case '+':
+          values[0] += values[1]
+          break
+        case '-':
+          values[0] -= values[1]
+          break
+        case '*':
+          values[0] *= values[1]
+          break
+        case '/':
+          values[0] /= values[1]
+          break
+        default:
+        }
+        
+      
+        values[1] = 0
+
+        setState({
+        displayValue: values[0],
+        operation: equals ? null : n,
+        current: equals ? 0: 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   function addDigit(n) {
-    console.log(n)
+    if (n === '.' && state.displayValue.includes('.')){
+      return
+    }
+    
+    let values = [...state.values]
+    const i = state.current
+    console.log(i)
+
+    const clearDisplay = state.displayValue === '0'
+      || state.clearDisplay
+    const currentValue = clearDisplay ? '' : state.displayValue
+    const displayValue = currentValue + n
+
+    values[i] = parseFloat(displayValue)
+
+    setState({displayValue, clearDisplay: false, values})
+
+    console.log(values)
   }
 
 
@@ -33,22 +83,22 @@ function Calculator() {
   return <Container>
     <Display value={state.displayValue}/>
     <Button label="AC" click={() => clearMemory()} triple/>
-    <Button label="/" click={n => setOperation(n)} operation/>
-    <Button label="7" click={n => addDigit(n)} />
-    <Button label="8" click={n => addDigit(n)} />
-    <Button label="9" click={n => addDigit(n)} />
-    <Button label="*" click={n => setOperation(n)} operation/>
-    <Button label="4" click={n => addDigit(n)} />
-    <Button label="5" click={n => addDigit(n)} />
-    <Button label="6" click={n => addDigit(n)} />
-    <Button label="-" click={n => setOperation(n)} operation/>
-    <Button label="1" click={n => addDigit(n)} />
-    <Button label="2" click={n => addDigit(n)} />
-    <Button label="3" click={n => addDigit(n)} />
-    <Button label="+" click={n => setOperation(n)} operation/>
-    <Button label="0" click={n => addDigit(n)} double/>
-    <Button label="." click={n => addDigit(n)} />
-    <Button label="=" click={n => setOperation(n)} operation/>
+    <Button label="/" click={setOperation} operation/>
+    <Button label="7" click={addDigit} />
+    <Button label="8" click={addDigit} />
+    <Button label="9" click={addDigit} />
+    <Button label="*" click={setOperation} operation/>
+    <Button label="4" click={addDigit} />
+    <Button label="5" click={addDigit} />
+    <Button label="6" click={addDigit} />
+    <Button label="-" click={setOperation} operation/>
+    <Button label="1" click={addDigit} />
+    <Button label="2" click={addDigit} />
+    <Button label="3" click={addDigit} />
+    <Button label="+" click={setOperation} operation/>
+    <Button label="0" click={addDigit} double/>
+    <Button label="." click={addDigit} />
+    <Button label="=" click={setOperation} operation/>
   </Container>
 }
 
