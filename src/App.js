0b8cb20499ef12f9b1ea04b6fa2a7dbd3000306f
Eric Soares Silva <ericsoares51@hotmail.com>
2023-03-23 20:32:38 -0300

    Added html in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Calculator from './components/Calculator'
-import GlobalStyles from './styles/GlobalStyles'
+import Calculator from "./components/Calculator";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
   return (
     <>
       <Calculator />
       <GlobalStyles />
+      <div>
+        <h1>Alteração</h1>
+      </div>
     </>
   );
 }
